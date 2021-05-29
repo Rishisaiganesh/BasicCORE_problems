4e745d60bd08ad5dac2dee24a2fe965910e5a221
Rishisaiganesh <crazyganesh67@gmail.com>
2021-05-29 23:38:44 +0530

    [Rishi]Adding:leapyear

--- Basic_core_problemS/Program.cs
+++ Basic_core_problemS/Program.cs
@@ -9,12 +9,17 @@ namespace Basic_core_problemS
             Console.WriteLine("Welcome to basic core programs ");
             Console.WriteLine("Enter your choice");
             Console.WriteLine("Press 1: to cheak HeadorTails");
+            Console.WriteLine("Press 2 : To check Its a LeapYear or Not");
             int selection = Convert.ToInt32(Console.ReadLine());
             switch (selection)
             {
                 case 1:
                     HeadorTails flip = new HeadorTails();
                     flip.Toss();
+                    break;
+                case 2:
+                    leapyear leap = new leapyear();
+                    leap.Leap();
                     break;
             }
         }
---
+++ Basic_core_problemS/leapyear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_core_problemS
+{
+    class leapyear
+    {
+        public void Leap()
+        {
+            Console.WriteLine("Enther a year to check if it isa LeapYear:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                Console.WriteLine("it is a leap year" + year);
+            else
+                Console.WriteLine("it is not Leap year" + year);
+
+        }
+    }
+}
