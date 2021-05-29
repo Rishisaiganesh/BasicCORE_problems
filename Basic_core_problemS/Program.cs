f205188e71f3cbe8be8c986bef55f3452701d037
Rishisaiganesh <crazyganesh67@gmail.com>
2021-05-29 23:32:11 +0530

    [Rishi]Adding:HeadorTails

---
+++ Basic_core_problemS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Basic_core_problemS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to basic core programs ");
+            Console.WriteLine("Enter your choice");
+            Console.WriteLine("Press 1: to cheak HeadorTails");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    HeadorTails flip = new HeadorTails();
+                    flip.Toss();
+                    break;
+            }
+        }
+    }
+}
+
